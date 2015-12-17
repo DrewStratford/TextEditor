ad1683b6629fe23dc36c1cc6c1b0a102e418fe93
Drew Stratford <drewstratford@outlook.com>
2015-12-17 18:25:14 +1300

    Reimplemented lineTracker using the PrefixTree, which should allow for faster jumps across lines. However column movement still needs to be fixed.

--- src/Data/PrefixTree.hs
+++ src/Data/PrefixTree.hs
@@ -3,11 +3,16 @@ module Data.PrefixTree
     ( PrefixTree
     , splitAtIndex
     , prefixAt
+    , insertAt
+    , valueAt
+    , module Data.FingerTree 
+    , size
     ) where
 
 import Data.FingerTree
 import Data.Monoid
 
+        
 instance Measured (Sum Int, Sum Int) Int where
     measure a = (Sum a, Sum 1)
 
@@ -17,12 +22,33 @@ test :: PrefixTree
 test = fromList [1,3,4,1,4,-1,9]
 
 splitAtIndex :: Int -> PrefixTree -> (PrefixTree, PrefixTree)
-splitAtIndex index tree = split (\(_,i) -> i > Sum index) tree
+splitAtIndex index  = split (\(_,i) -> i-1 > Sum index) 
 
+-- this could potentiall be improved 
 prefixAt :: Int -> PrefixTree -> Int
 prefixAt index tree =
     case a of
-      EmptyR -> 0
+      EmptyR   -> 0
       (_ :> b) -> b
     where (left,_ ) = splitAtIndex index tree
-          a = viewr $ fmapWithPos (\(Sum x,_) _ -> x) left
+          a         = viewr $ fmapWithPos (\(Sum x,_) _ -> x) left
+
+
+valueAt :: Int -> PrefixTree -> Int
+valueAt index tree =
+    case a of
+      EmptyR   -> 0
+      (_ :> b) -> b
+    where (left,_ ) = splitAtIndex index tree
+          a         = viewr left
+
+insertAt :: Int -> Int -> PrefixTree -> PrefixTree
+insertAt index insertee pTree = (left |> insertee) >< right
+  where (left, right) = splitAtIndex index pTree
+
+size :: PrefixTree -> Int
+size pTree =
+  case sTree of
+    EmptyR      -> 0
+    (_ :> size) -> size
+  where sTree = viewr $ fmapWithPos (\(Sum x, _) y -> x + y) pTree
--- src/LineTracker.hs
+++ src/LineTracker.hs
@@ -7,68 +7,77 @@ module LineTracker
     , cursorRight
     , newline
     , asSeqCoOrd
-    , line
     , lineLength
+    , line
     , col
     , increase
     , decrease
     ) where
 
 import Prelude hiding (lines)
-import Zip.Zip
-
+import Data.PrefixTree
+    
 data LineTracker = LineTracker
     {
-      lines         :: Zip Int,
-      offset :: Int,
+      lines         :: PrefixTree,
+      offset        :: Int,
+      lineLength    :: Int,
       line          :: Int,
       col           :: Int
     } deriving Show 
 
-lineTracker = LineTracker ( 0 `insert` zipper) 0 0 0
+lineTracker :: LineTracker
+lineTracker = LineTracker (insertAt 0 0 empty) 0 0 0 0 
 
-cursorUp cursor
-         -- we do left because the when we are at the first line we'll still
-         -- have one int int the left side of the zipper and atStart would return false
-    | atStart $ left $ lines cursor = cursor
-    | otherwise = nCurs { offset = off,col = nCol}
-  where off = offset cursor - lineLength nCurs
-        lineNum = line cursor - 1
-        nCol = min (col nCurs) (lineLength nCurs - 1)
-        nCurs = cursor {  line = lineNum, lines = left (lines cursor)}
+changeLine :: Int -> LineTracker -> LineTracker
+changeLine i cursor
+    | i < 0  = cursor
+    | i > size (lines cursor) = cursor
+    | otherwise =
+        cursor{ offset = off
+              , col    = newCol
+              , line   = i
+              , lineLength = newLen
+              }
+  where off    = prefixAt i (lines cursor)
+        newCol = min newLen  (col cursor)
+        newLen = valueAt i $ lines cursor
+                 
+cursorUp cursor = changeLine l cursor
+  where l = line cursor - 1
 
-cursorDown cursor
-    | atEnd $ lines cursor = cursor
-    | otherwise = nCurs { offset = off, col = nCol}
-  where off = offset cursor + lineLength cursor
-        lineNum = line cursor + 1
-        nCurs = cursor { offset = off, line = lineNum, lines = right (lines cursor)}
-        nCol
-           | atEnd $ lines nCurs = min (col nCurs) (lineLength nCurs )
-           | otherwise            = min (col nCurs) (lineLength nCurs - 1)
+cursorDown cursor = changeLine l cursor
+  where l = line cursor + 1
 
 cursorLeft cursor = cursor { col = max (col cursor -1) 0 }
 cursorRight cursor = cursor { col = min (col cursor +1) len }
-  where len
-           | atEnd $ lines cursor = lineLength cursor
-           -- we minus one because we don't want it going passed the newline
-           | otherwise = lineLength cursor - 1
+  where len = lineLength cursor
               
 newline :: LineTracker -> LineTracker
-newline cursor = cursorDown cursor
-                 { col = 0
-                   -- Updates line size and inserts remainder in newline
-                 , lines = left $ r `insert` toPoint (const l) (lines cursor)
-                 }
-  -- splits line based on cursor 
-  where (l, r) = (col cursor, lineLength cursor - col cursor) 
-        
-increase cursor = cursor{ col = col cursor + 1, lines = toPoint (+1) $ lines cursor}
-decrease cursor = cursor{ col = col cursor - 1, lines = toPoint (\x -> x- 1) $ lines cursor}
+newline cursor =  cursor {
+                    lines =(left |> split |> newLineLen) >< right,
+                    col   = 0,
+                    line  = line cursor + 1,
+                    offset = off + split,
+                    lineLength = newLineLen
+                    }
+  where (l, right) = splitAtIndex (line cursor) (lines cursor)
+        (left, _ ) = splitAtIndex (line cursor -1) l
+        split      = lineLength cursor - newLineLen
+        newLineLen = lineLength cursor - col cursor
+        off        = prefixAt (line cursor) $ lines cursor
+                  
+increase cursor = cursor {
+                    lineLength = max 0 $ lineLength cursor + 1,
+                    col = col cursor + 1
+                    }
+decrease cursor = cursor {
+                    lineLength = lineLength cursor + 1,
+                    col = col cursor - 1
+                    }
 
 asSeqCoOrd :: LineTracker -> Int
 asSeqCoOrd lTracker = offset lTracker + col lTracker
 
 
-lineLength  = withPoint id 0 . lines
 
