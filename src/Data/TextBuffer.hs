6c8f8f9658495dcf71d4b37f2359420f40576bc6
Drew Stratford <drewstratford@outlook.com>
2016-01-05 15:16:33 +1300

    Got the text movements working. However setCursorPosition does change where the cursor is displayed in urxvt (however text is placed correctly).

--- src/Data/TextBuffer.hs
+++ src/Data/TextBuffer.hs
@@ -1,25 +1,51 @@
 module Data.TextBuffer
-    () where
+    ( TextBuffer
+    , insert
+    , backspace
+    , newline
+    , moveCol
+    , moveRight
+    , moveLeft
+    , moveLine
+    , getLineCol
+    , fromStrings
+    , getLineSection
+    ) where
 
-import Prelude hiding (drop, splitAt,length, lines)
+import Prelude hiding (drop, splitAt,length, lines, take)
 import Data.Sequence
 
 type Line = Seq Char
     
 type TextBuffer = ( Seq Line, Line, Line, Seq Line)
 
+
+-- | construction
+fromStrings :: [String] -> TextBuffer
+fromStrings ss = splitAtLine (fromList $ map fromList ss) 0
+
 ----------------------------------------------------------------------------
 insert (l,il,ir,r) c = (l,il |> c, ir,r)
 
 backspace text@(l,il,ir,r) =
   let (line, col) = getLineCol text
-      il'         = drop 1 il
+      (il', _)    = splitAt (col - 1) il
   in case col of
        0 -> mergeWithPrev text
        _ -> (l, il', ir, r)
 
+newline (l,il,ir,r) = (l |> il, empty, ir,r)
+
 moveCol (i,il,ir,r) col = (i,il',ir',r)
   where (il',ir')  = splitAt col (il><ir)
+
+moveLeft (i,il,ir,r) = (i,il',ir',r)
+  where (il',c) = splitAt (length il - 1) il
+        ir'     = c >< ir
+        
+moveRight (i,il,ir,r) = (i,il',ir',r)
+  where (c,ir') = splitAt 1 ir
+        il'     = il >< c
 
 moveLine tb@(_,il,_,_) line = moveCol (splitAtLine (merge tb) line) col
   where col = length il
@@ -42,12 +68,23 @@ getLineCol (l,il,_,_) = (length l, length il)
 
 -- | merges the current line with the previous line
 --   the current line is now the prev line + old current line
+mergeWithPrev :: TextBuffer -> TextBuffer
 mergeWithPrev tb@(l, il, ir, r) =
   case viewr l of
     EmptyR -> tb
     (l' :> il') -> (l', il' >< il, ir, r)
                      
+
+----------------------------------------------------------------------------
+-- | draws the section of the textbuffer specified
+getLineSection :: Int -> Int -> TextBuffer -> Seq Line
+getLineSection x height textBuf =
+  let text = merge textBuf
+      (_,_,ir,r) = splitAtLine text x
+      (segment,_,_,_) = splitAtLine (ir <| r) height
+  in segment
+
 ----------------------------------------------------------------------------
 --helpers
-merge (l,il,ir,r) = (l |> il) >< (ir <| r)
+merge (l,il,ir,r) = (l |> (il >< ir)) >< r
 
--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@ import Data.Foldable
     
 import System.IO 
 import System.Console.ANSI
+import qualified Data.Sequence as S
 
 import UI.HSCurses.Curses
 import UI.HSCurses.CursesHelper
@@ -11,62 +12,73 @@ import Data.TextBuffer
 main :: IO ()
 main = do
   file <- readFile "src/Main.hs"
-  textBuffer <- return $ fromString file
-  initCurses
-  --raw True
-  --cBreak True
-  echo False
-  window <- initScr
-  
-  (loop window textBuffer)
-  endWin   -- closes everything
+  textBuffer <- return $ fromStrings $ lines file
+  hSetBuffering stdin NoBuffering
+  hSetEcho stdin False
+  (loop textBuffer)
 
-loop :: Window -> TextBuffer -> IO ()
-loop win text = do
-  input <- getch
-  move 0 0 
-  case decodeKey input of
-    KeyChar '\DEL' -> do
-                  t <- return $ remove text
-                  drawLine 800 (toString t)
+loop :: TextBuffer -> IO ()
+loop text = do
+  input <- getChar
+  clearScreen 
+  cursPos 0 0 
+  case input of
+    '\DEL'     -> do
+                  t <- return $ backspace text
                   (x, y) <- return $ getLineCol t
-                  move y x
-                  wRefresh win
-                  loop win t
-    KeyEnter     -> do
-                  t <- return $ text `insert` '\n'
-                  drawLine 800 (toString t)
+                  drawSection t 0 0 40 40
+                  cursPos x y
+                  loop t
+    '\n'       -> do
+      ---- fix this to split over lines
+                  t <- return $ newline text
                   (x, y) <- return $ getLineCol t
-                  move y x
-                  wRefresh win
-                  loop win t
-    KeyChar 'Q'  -> return ()
-    KeyChar  c   -> do
+                  drawSection t 0 0 40 40
+                  cursPos x y
+                  loop t
+    'L'        -> do
+                  (x, y) <- return $ getLineCol text
+                  t <- return $ moveRight text
+                  drawSection t 0 0 40 40
+                  (x, y) <- return $ getLineCol text
+                  cursPos x y
+                  loop t
+    'K'        -> do
+                  (x, y) <- return $ getLineCol text
+                  t <- return $ text `moveLine` (x -1)
+                  drawSection t 0 0 40 40
+                  (x, y) <- return $ getLineCol text
+                  cursPos x y
+                  loop t
+    'J'        -> do
+                  (x, y) <- return $ getLineCol text
+                  t <- return $ text `moveLine` (x +1)
+                  drawSection t 0 0 40 40
+                  (x, y) <- return $ getLineCol text
+                  cursPos x y
+                  loop t
+    'H'        -> do
+                  (x, y) <- return $ getLineCol text
+                  t <- return $ moveLeft text
+                  drawSection t 0 0 40 40
+                  (x, y) <- return $ getLineCol text
+                  cursPos x y
+                  loop t
+                  
+    'Q'          -> return ()
+    c            -> do
                   t <- return $ text `insert` c
-                  drawLine 800 (toString t)
                   (x, y) <- return $ getLineCol t
-                  move y x
-                  wRefresh win
-                  loop win t
-    _            -> loop win text
+                  drawSection t 0 0 40 40
+                  cursPos x y
+                  loop t
                     
+drawSection :: TextBuffer -> Int -> Int -> Int -> Int -> IO ()
+drawSection text x y width height = do
+  let lines = getLineSection y height text
+  mapM_ ((\cs -> mapM_ putChar cs >> print '\n' ) . toList . S.take width . S.drop x) lines
+
+cursPos :: Int -> Int -> IO ()
+cursPos l c = putStr $ "\ESC[" ++ show l ++ ";" ++ show c ++ "H"
 {-
-update :: TextBuffer -> IO ()
-update doc = do
-  c <- getChar
-  case c of
-    '\DEL'   -> do
-           d <- return $ remove doc
-           clearScreen
-           setCursorPosition 0 0
-           _ <- output d
-           hFlush stdout
-           update d
-    _   -> do
-           d <- return $ doc `insert` c
-           clearScreen
-           setCursorPosition 0 0
-           _ <- output d
-           hFlush stdout
-           update d
 -}
